61612fdbcb3a1c76b8a47235a373cfb1482212a6
Dries Van De Putte <dvandepu@uwo.ca>
2024-09-16 17:04:04 -0400

    Copy jobfile creation from removed parallel_tools to main script

--- pdrs4all/pipeline_run.py
+++ pdrs4all/pipeline_run.py
@@ -13,80 +13,101 @@ print("JWST pipeline version", jwst.__version__)
 print("CRDS context version", get_crds_env_context())
 
 
-class InstrumentsPipelines:
-    def __init__(self, args):
-        """
-        args: all the things read in by parser.py
+def create_strun_jobs(args):
+    """Parse default options, custom options, and set up strun
+    commands.
 
-        """
-        self.stage_numbers = [i for i in (1, 2, 3) if str(i) in args.steps]
-        self.obs_dir = args.source_dir
-        self.imp_dir = args.imp_dir
-        self.back_dir = args.back_dir
-        self.custom_options = args.custom_options
-        obsfile = sorted(Path(self.obs_dir).glob("*.fits"))[0]
-        self.out_dir = (
-            str(Path(self.obs_dir) / "..")
-            if args.output_dir is None
-            else args.output_dir
-        )
-        self.intermediate_dir = (
-            self.out_dir if args.intermediate_dir is None else args.intermediate_dir
-        )
-        self.per_pointing = not args.mosaic
-        # NRC_IMAGE, MIR_MRS, MIR_IMAGE
-        with datamodels.open(obsfile) as im:
-            self.instru = im.meta.exposure.type
+    Output
+    ------
+    Writes job file containing one strun command per input file.
 
-        self.is_spectroscopy = self.instru == "MIR_MRS" or self.instru == "NRS_IFU"
+    (Per association file in the case of stages 2 and 3)
 
-    def run_pipeline(self, max_cores=1):
-        # run requested stages
-        for stage in self.stage_numbers:
-            # set up output path and make subdir
-            output_path = Path(self.out_dir) / f"stage{stage}/"
-            output_path.mkdir(parents=True, exist_ok=True)
+    """
 
-            # choose input files
-            if stage == 1:
-                inputs = sorted(
-                    [str(p) for p in Path(self.obs_dir).glob("*_uncal.fits")]
-                )
-            elif stage == 2:
-                inputs = create_association.create_asn(
-                    self.intermediate_dir,
-                    "stage1/*_rate.fits",
-                    level=2,
-                    backdir=self.back_dir,
-                    impdir=self.imp_dir,
-                    output_dir=str(Path(self.intermediate_dir) / "stage1/"),
-                )
-            elif stage == 3:
-                inputs = create_association.create_asn(
-                    self.intermediate_dir,
-                    "stage2/*_cal.fits",
-                    level=3,
-                    backdir=self.back_dir,
-                    spectroscopy=self.is_spectroscopy,
-                    per_pointing=self.per_pointing,
-                    output_dir=str(Path(self.intermediate_dir) / "stage2/"),
-                )
+    stage_numbers = [i for i in (1, 2, 3) if str(i) in args.steps]
+    args.custom_options = args.custom_options
+    obsfile = sorted(Path(args.source_dir).glob("*.fits"))[0]
+    out_dir = (
+        str(Path(args.source_dir) / "..")
+        if args.output_dir is None
+        else args.output_dir
+    )
+    intermediate_dir = (
+        out_dir if args.intermediate_dir is None else args.intermediate_dir
+    )
+    per_pointing = not args.mosaic
 
-            # get pipeline, options, and run
-            pipeline, options = pipeline_class_and_options_dict(
-                stage, self.instru, str(output_path)
+    # NRC_IMAGE, MIR_MRS, MIR_IMAGE
+    with datamodels.open(obsfile) as im:
+        instru = im.meta.exposure.type
+
+    is_spectroscopy = instru == "MIR_MRS" or instru == "NRS_IFU"
+
+    # run requested stages
+    for stage in stage_numbers:
+        # set up output path and make subdir
+        output_path = Path(out_dir) / f"stage{stage}/"
+        output_path.mkdir(parents=True, exist_ok=True)
+
+        # choose input files
+        if stage == 1:
+            inputs = sorted(
+                [str(p) for p in Path(args.source_dir).glob("*_uncal.fits")]
             )
-            # user-defined options, just for this run (not in default pipeline_settings.py)
-            if self.custom_options is not None:
-                print("Applying custom settings from", self.custom_options)
-                apply_custom_options(options, self.custom_options)
-                print("Final options are: ", options)
+        elif stage == 2:
+            inputs = create_association.create_asn(
+                intermediate_dir,
+                "stage1/*_rate.fits",
+                level=2,
+                backdir=args.back_dir,
+                impdir=args.imp_dir,
+                output_dir=str(Path(intermediate_dir) / "stage1/"),
+            )
+        elif stage == 3:
+            inputs = create_association.create_asn(
+                intermediate_dir,
+                "stage2/*_cal.fits",
+                level=3,
+                backdir=args.back_dir,
+                spectroscopy=is_spectroscopy,
+                per_pointing=per_pointing,
+                output_dir=str(Path(intermediate_dir) / "stage2/"),
+            )
+
+        # get pipeline type and options
+        pipeline, options = pipeline_class_and_options_dict(
+            stage, instru, str(output_path)
+        )
+        # user-defined options, just for this run (not in default pipeline_settings.py)
+        if args.custom_options is not None:
+            print("Applying custom settings from", args.custom_options)
+            apply_custom_options(options, args.custom_options)
+            print("Final options are: ", options)
+
+        # convert options dict to command line options string
+        strun_command_base = f"strun {pipeline.class_alias}"
+        strun_options = ""
+        for opt_k, opt_v in options.items():
+            if opt_k == "steps":
+                for step, step_pars in opt_v.items():
+                    for par, par_v in step_pars.items():
+                        strun_options += f" --steps.{step}.{par}={par_v}"
+            else:
+                strun_options += f" --{opt_k}={opt_v}"
+
+        job_fname = f"strun_{pipeline.class_alias}_jobs.sh"
+        with open(job_fname, "w") as f:
+            for input_fn in inputs:
+                strun_command = f"{strun_command_base} {input_fn} {strun_options}"
+                f.write(strun_command + "\n")
+
+        print("wrote job file to ", job_fname)
 
 
 def main():
     args = parse_args()
-    init = InstrumentsPipelines(args)
-    init.run_pipeline(args.max_cores)
+    create_strun_jobs(args)
 
 
 if __name__ == "__main__":
